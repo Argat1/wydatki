396dda25625222c642d4ebac0dd37e3da685e619
lukasz jablonski <lukasz.jablonski44@gmail.com>
2024-03-11 10:39:06 +0100

    added_class_wydatek

---
+++ wydatki/wydatki/Wydatek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wydatki
+{
+    internal class Wydatek
+    {
+        int id {  get; set; }
+        DateTime data { get; set; }
+        string nazwa { get; set; }
+        double kwota { get; set; }
+            
+    }
+}
