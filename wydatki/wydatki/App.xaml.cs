d8e0c9bf9c3e60e050220733f6a97edae8b9b386
lukasz jablonski <lukasz.jablonski44@gmail.com>
2024-03-20 09:25:09 +0100

    Added front end to main page

--- wydatki/wydatki/App.xaml.cs
+++ wydatki/wydatki/App.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
 
 namespace wydatki
 {
     public partial class App : Application
     {
+         static string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "baza.txt");
+
+        
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- wydatki/wydatki/MainPage.xaml.cs
+++ wydatki/wydatki/MainPage.xaml.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace wydatki
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
+        
+
         public MainPage()
         {
             InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            wyswietl();
+        }
+        private void wyswietl()
+        {
+            
+            
+        }
+        public async void DodajWydatek(object sender, EventArgs e)
+        {
+            
+        }
+        public async void Sczegoly(object sender, EventArgs e)
+        {
+            
         }
     }
 }
